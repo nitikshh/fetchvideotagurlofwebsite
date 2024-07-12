74c55e4e424b4f5f65b5307d1ce21690fc1bbffc
Nitiksh Kumar <122013711+nitikshh@users.noreply.github.com>
2024-07-12 09:05:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+import random
 from selenium.webdriver.support import expected_conditions as EC
 
 app = Flask(__name__)
@@ -49,4 +50,4 @@ def get_video_url():
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(host='0.0.0.0', port=random.randint(2000, 9000), debug=True)
